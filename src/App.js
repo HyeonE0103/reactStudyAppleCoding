1dac02fa81b3a5fa071937af26029aaaa294b862
HyeonE <mag30102000@gmail.com>
2023-02-01 23:59:58 +0900

    버튼 기능개발과 리액트 state 변경하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+/* eslint-disable */
+
 import "./App.css";
 import { useState } from "react";
 
 function App() {
   let post = "배고파";
-  let [글제목, b] = useState("바지락 칼국수 먹고싶다");
+  let [글제목, b] = useState([
+    "바지락 칼국수 먹고 싶다",
+    "대창 먹고 싶다",
+    "초콜릿 먹고 싶다",
+  ]);
+  let [좋아요, 좋아요변경] = useState(0);
+
+  function 함수() {}
 
   return (
     <div className="App">
@@ -11,7 +20,27 @@ function App() {
         <div>개발 blog</div>
       </div>
       <div className="list">
-        <h4>{글제목}</h4>
+        <h4>
+          {글제목[0]}{" "}
+          <span
+            onClick={() => {
+              좋아요변경(좋아요 + 1);
+            }}
+          >
+            👍
+          </span>{" "}
+          {좋아요}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
