7abd165e75767f09c6b7506f0e06a25a3aa6d6d9
HyeonE <mag30102000@gmail.com>
2023-02-10 23:58:06 +0900

    map을 이용하여 div를 반복으로 줄이기

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let post = "배고파";
   let [글제목, 음식변경] = useState([
     "바지락 칼국수 먹고 싶다",
     "대창 먹고 싶다",
     "초콜릿 먹고 싶다",
   ]);
-  let [좋아요, 좋아요변경] = useState(0);
+
+  let [좋아요, 좋아요변경] = useState([0, 0, 0]);
 
   let [modal, setModal] = useState(false);
 
@@ -21,34 +21,31 @@ function App() {
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
-      <div className="list">
-        <h4
-          onClick={() => {
-            modal == true ? setModal(false) : setModal(true);
-          }}
-        >
-          {글제목[0]}{" "}
-          <span
-            onClick={() => {
-              좋아요변경(좋아요 + 1);
-            }}
-          >
-            👍
-          </span>{" "}
-          {좋아요}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-
-      <div className="list">
-        <h4>{글제목[2]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
+      {글제목.map(function (a, i) {
+        return (
+          <div className="list" key={i}>
+            <h4
+              onClick={() => {
+                modal == true ? setModal(false) : setModal(true);
+              }}
+            >
+              {글제목[i]}
+              {/* 글제목[0], 글제목[1] ...이 순서대로 들어감 */}
+              <span
+                onClick={() => {
+                  let copy = [...좋아요];
+                  copy[i] = copy[i] + 1;
+                  좋아요변경(copy);
+                }}
+              >
+                👍
+              </span>
+              {좋아요[i]}
+            </h4>
+            <p>2월 18일 발행</p>
+          </div>
+        );
+      })}
       {modal == true ? <Modal /> : null}
     </div>
   );
